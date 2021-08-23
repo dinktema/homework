b07aa95e83ebbf1fa9c24fcb4daec5cc4d9aba7c
dinktema <ctrlaltkil@gmail.com>
2021-08-23 21:45:26 +0300

    add sec_methodis_leap_year calculation add coins_sum

--- coins/coins.py
+++ coins/coins.py
@@ -5,12 +5,22 @@
 # Номинальная стоимость монет: a, b и с тугриков.
 
 def max_sum_of_2(a: int, b: int, c: int) -> int:
-    pass
-
+    sum1 = a + b
+    sum2 = a + c
+    sum3 = b + c
+    if sum1 > sum2 and sum1 > sum3:
+        return sum1
+    if sum2 > sum1 and sum2 > sum3:
+        return sum2
+    if sum3 > sum1 and sum3 > sum2:
+        return sum3
+    else:
+        print("sums of all coins is same")
 
 if __name__ == "__main__":
     a = int(input("a = "))
-    b = int(input("a = "))
-    c = int(input("a = "))
-
-    print(max_sum_of_2(a, b, c))
+    b = int(input("b = "))
+    c = int(input("c = "))
+    if a <= 0 or b <= 0 or c <= 0:
+        raise Exception("invalid inout data")
+    print("max sum of two coins = " + str(max_sum_of_2(a, b, c)))
--- pizza_delivery/pizza.py
+++ pizza_delivery/pizza.py
@@ -16,7 +16,7 @@ def count_flats_in_entrance(floors, flats_on_floor):
     return count_flats
 
 def find_entrance(flat_num, count_flats):
-    entrance = flat_num / count_flats
+    entrance = flat_num // count_flats
     if entrance % 2 != 0 and entrance % 2 != 1:
         entrance = (flat_num // count_flats) + 1
     return int(entrance)
