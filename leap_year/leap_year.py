4ec1cfe892bcda58792260d6fae3268a0381cce9
dinktema <ctrlaltkil@gmail.com>
2021-08-23 19:27:01 +0300

    add is_leap_year calculation

--- leap_year/leap_year.py
+++ leap_year/leap_year.py
@@ -19,10 +19,15 @@
 
 
 def is_leap_year(year: int) -> bool:
-    pass
-
+    if year % 4 == 0 and year % 100 != 0:
+            return True
+    elif year % 400 == 0:
+            return True
+    else:
+        return False
 
 if __name__ == "__main__":
     year = int(input("Введите год: "))
-
+    if year <= 0:
+        raise Exception("invalid inout data")
     print(is_leap_year(year))
