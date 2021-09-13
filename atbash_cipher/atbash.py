ec0f17e547d0c727b4cea7a5aa38ab2624dde9a1
dinktema <ctrlaltkil@gmail.com>
2021-09-13 18:24:55 +0300

    start chipher

--- atbash_cipher/atbash.py
+++ atbash_cipher/atbash.py
@@ -2,7 +2,8 @@
 
 """
 Напишите программу, которая кодирует и декодирует текст шифром Атбаш.
-В этом шифре каждая i-ая буква алфавита заменяется i-ой буквой с его конца, например, для латинского алфавита: a - z, b - y и т.д.
+В этом шифре каждая i-ая буква алфавита заменяется i-ой буквой с его конца, например, для латинского алфавита: a - z,
+ b - y и т.д.
 
 - заглавные буквы переводятся в строчные
 - пробельные символы и знаки препинания опускаются
@@ -14,11 +15,36 @@
 """
 
 BLOCK_SIZE = 5
+import string
 
 
 def atbash_encode(text: str) -> str:
-    pass
+    words = text.split()
+    cipher = ''
+    for word in words:
+        word = word.lower()
+        for letter in word:
+             if letter in string.ascii_lowercase:
+                letind = string.ascii_lowercase.index(letter) + 1  #-> int
+                newletter = list(string.ascii_lowercase)[len(string.ascii_lowercase) - letind] #-> str
+                cipher += newletter
+        cipher += ' '
+    return cipher
 
 
 def atbash_decode(cipher: str) -> str:
-    pass
+    cipher = cipher.split()
+    text = ''
+    for word in cipher:
+        for letter in word:
+            if letter in string.ascii_lowercase:
+                letind = string.ascii_lowercase.index(letter) +1  # -> int shifer
+                newletter = list(string.ascii_lowercase)[len(string.ascii_lowercase) - letind]  # -> str
+                text += newletter
+        text += ' '
+    return text
+
+cipfer = 'dzi rh kvzxv uivvwln rh hozevib rtmlizmxv rh hgivmtgs '
+text = 'War is Peace,\t Freedom is Slavery,\n Ignorance is Strength.'
+print(atbash_encode(text))
+print(atbash_decode(cipfer))
